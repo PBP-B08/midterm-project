e12ae8f090ae89a2602dd14bde2dd39f3720a14a
Vicky Maulana <muhammad.vicky@ui.ac.id>
2022-10-17 01:03:52 +0700

    create register, login, logout

---
+++ main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from main.views import index, login_user, register, logout_user
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('login/', login_user, name='login'),
+    path('register/', register, name='register'),
+    path('logout/', logout_user, name='logout'),
+]
