90afab22fb3cf9e9cb4f21f6e8f9359d3d3987d8
mtrq <muhammad.tarreq@ui.ac.id>
2022-10-29 15:08:29 +0700

    Initiate PlanYourTrip

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from main.views import index, login_user, register, logout_user
 
 app_name = 'main'
@@ -8,4 +8,10 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('register/', register, name='register'),
     path('logout/', logout_user, name='logout'),
+    path('profile/', profile, name='profile'),
+    path('profile/update/', update_profile, name='update_profile'),
+    path('recommendation/', include('recommendation.urls')),
+    path('event-calendar/', include('eventcalendar.urls')),
+    # path('things-to-do/', include('things_to_do.urls')),
+    path('plan/', include('PlanYourTrip.urls')),
 ]
