1d0515733559b3249a4f56d6c809fb05d8be4dbe
GEdgar <gabrieledgarfn@gmail.com>
2022-11-02 20:42:40 +0700

    Last Effort Calendar

--- eventcalendar/admin.py
+++ eventcalendar/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import Location, Event
+from .models import EventCalendar
 # Register your models here.
-admin.site.register(Location)
-admin.site.register(Event)
+admin.site.register(EventCalendar)
