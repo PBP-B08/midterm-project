1aece9c55b6eb9edbaf5b516bbfd44927a7781ad
Vicky Maulana <muhammad.vicky@ui.ac.id>
2022-11-28 20:31:30 +0700

    fix csrf error

--- project_django/settings.py
+++ project_django/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = 'django-insecure-3@5wx%(^*zl68l(o$^m-3%cvjv0g&mom1ra=oj5f048_al57s7
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = [f'{APP_NAME}.up.railway.app']
 
 # Application definition
 
@@ -147,4 +148,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = ""
 MEDIA_URL = ""
 
-CSRF_TRUSTED_ORIGINS = ['https://cultural-center.up.railway.app/']
+# CSRF_TRUSTED_ORIGINS = ['https://cultural-center.up.railway.app/']
